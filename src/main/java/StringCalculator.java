f6759b8fed50fc2419494e3fc6d173ae57a89680
david <davidomej@hotmail.com>
2022-04-20 21:36:50 +0100

    Test differentDelimitersReturnSum RED

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,18 +1,18 @@
 public class StringCalculator {
 
     public int add(String s) {
-        String[] numbers = s.split(",");
+        String[] numbers = s.split("//|,|\n|;");
         int n = 0;
+
         if(s==""){
             n=0;
-        }
-        if(s.length()==1) {
-            n=Integer.parseInt(s);
-        }
-        if(numbers.length>=2){
+        }else if(s.length()==1){
+                n=Integer.parseInt(s);
+        }else if(numbers.length==2){
             n= Integer.parseInt(numbers[0])+Integer.parseInt(numbers[1]);
+        }else if(numbers.length>2){
+            n = Integer.parseInt(numbers[0])+ Integer.parseInt(numbers[1])+ Integer.parseInt(numbers[2]);
         }
-
         return n;
     }
 
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -4,21 +4,40 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StringCalculatorShould {
 
+    //String = "" Empty String
     @Test
     void emptyStringReturnZero(){
         StringCalculator calc = new StringCalculator();
         assertEquals(calc.add(""), 0);
     }
 
+    //String = "1" Only one number
     @Test
     void singleStringReturnInt(){
         StringCalculator calc = new StringCalculator();
         assertEquals(calc.add("1"), 1);
     }
 
+    //String = "1,2" Two numbers separated with comma
     @Test
     void stringWithTwoNumbers(){
         StringCalculator calc = new StringCalculator();
         assertEquals(calc.add("1,2"), 3);
     }
+
+    //String = 1/n2,3 Three numbers separated with comma and new line.
+    @Test
+    void twoNumbersSeparatedByCommaAndMultiline(){
+        StringCalculator calc = new StringCalculator();
+        assertEquals(calc.add("1,2\n3"),6);
+    }
+
+    //String = //;\n1,2 Support different delimiters
+    @Test
+    void differentDelimitersReturnSum(){
+        StringCalculator calc = new StringCalculator();
+        assertEquals(calc.add("//;\n1,2"),3);
+    }
+
+
 }
